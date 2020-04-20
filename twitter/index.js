dc4d764df6c89c92025b718ce5889e5b773b3e59
StickyNeutrino <dfitc001@ucr.edu>
2020-04-20 12:37:10 +0000

    inital commit

---
+++ twitter/index.js
@@ -0,0 +1,39 @@
+const Twitter = require('twitter-lite');
+const mongodb = require('mongodb');
+
+var MongoClient = mongodb.MongoClient;
+var url = "mongodb://mongo/tweets"
+
+async function run() {
+
+const app = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_TOKEN_SECRET
+});
+
+function collectTweets( db ) {
+  function tweetHandler( tweet ) {
+    console.log( tweet.text ) ;
+  }
+
+  const params = {
+    locations: '-117.347213,32.559664,-116.905013,32.956942'
+  }
+
+  const stream = app.stream('statuses/filter', params)
+    .on("data", tweetHandler)
+}
+
+
+MongoClient.connect( url, function( err, db) { 
+  if ( err ) throw err;
+  console.log( "Attached to MongoDB" );
+  collectTweets( db );
+  db.close();
+});
+
+}
+
+run()
---
+++ twitter/node_modules/twitter-lite/dist/stream.m.js
@@ -0,0 +1,2 @@
+var t=function(t){function e(){t.call(this),this.buffer=""}return t&&(e.__proto__=t),(e.prototype=Object.create(t&&t.prototype)).constructor=e,e.prototype.parse=function(t){var e,r;for(this.buffer+=t.toString("utf8");(e=this.buffer.indexOf("\r\n"))>-1;)if(r=this.buffer.slice(0,e),this.buffer=this.buffer.slice(e+2),r.length>0)try{r=JSON.parse(r),this.emit(r.event||"data",r)}catch(t){t.source=r,this.emit("error",t)}else this.emit("ping")},e}(require("events"));module.exports=t;
+//# sourceMappingURL=stream.m.js.map
