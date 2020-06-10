0852e5e121a74fa9126315bb7f76836c02875712
StickyNeutrino <dfitc001@ucr.edu>
2020-06-10 22:59:38 +0000

    mvn and mongo start

---
+++ lucene/TweetLucene/src/main/java/cs172/project/TweetLucene/App.java
@@ -0,0 +1,23 @@
+
+package cs172.project.TweetLucene;
+
+import com.mongodb.MongoClient;
+
+
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+
+import org.bson.Document;
+import java.util.Arrays;
+
+
+public class App
+{
+    public static void main( String[] args )
+    {
+
+        MongoClient mongoClient = new MongoClient( "mongo" );
+        MongoDatabase database = mongoClient.getDatabase("tweets");
+        MongoCollection<Document> collection = database.getCollection("test");
+    }
+}
--- lucene/cs172/TweetLucene.java
+++
@@ -1,6 +0,0 @@
-package cs172;
-public class TweetLucene {
-  public static void main(String[] args) {
-System.out.println("hi dan");
-  }
-}
