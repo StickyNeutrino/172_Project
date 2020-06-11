348b787c2295b4c08697240db88728cfb38e8f4d
Jae <daniel.kwong.h@gmail.com>
2020-06-10 19:49:58 -0700

    pls

--- lucene/App.java
+++ lucene/App.java
@@ -23,13 +23,11 @@ import org.json.JSONObject;
 //package TweetLucene;
 
 class Tweet {
-	public String date;
-	public String coords;
-	public String user;
-	public String text;
-	public String hashtags;
-	public String link;
-	public String ptitle;
+	/**
+	 * Struct of tweet in JSON files created_at, location, screen_name, description,
+	 * hashtags, url, pageTitle
+	 */
+	public String date, coords, user, text, hashtags, link, ptitle;
 
 	Tweet(String d, String c, String u, String t, String h, String l, String p) {
 		this.date = d;
@@ -42,8 +40,8 @@ class Tweet {
 	}
 }
 
-public class App {	  
-	public static void main(String[] args) throws IOException, org.json.simple.parser.ParseException {			
+public class App {
+	public static void main(String[] args) throws IOException, org.json.simple.parser.ParseException {
 		BufferedReader b = null;
 		File file = new File("CS171_DATA");
 
@@ -57,90 +55,95 @@ public class App {
 		line = b.readLine();
 		while ((line != null)) {
 			System.out.println("HELLO");
-//          System.out.println(line);
-			Scanner tweetParts = new Scanner(line).useDelimiter(
-					"\\s*created_at:\\s*|\\s*location:\\s*|\\s*screen_name:\\s*|\\s*description:\\s*|\\s*hashtags:\\s*|\\s*url:\\s*|\\s*pageTitle:\\s*");
-//			Scanner tweetParts = new Scanner(line);
-			if (tweetParts == null) {
-				System.out.println("wat");
-			}
-			JSONObject obj = new JSONObject(line);
-			System.out.println(tweetParts.hasNext());
-//			System.out.println(obj.getString("created_at"));
-//			System.out.println(tweetParts.next().toString());
-			String date = tweetParts.next();
-			String coords = tweetParts.next();
-			String user = tweetParts.next();
-			String text = tweetParts.next();
-			String hashtags = tweetParts.next();
-//          String link = tweetParts.next();
-			String url = tweetParts.next();
-			String ptitle = tweetParts.next();
-//          String title = tweetParts.next();
-			Tweet tweet = new Tweet(date, coords, user, text, hashtags, url, ptitle);
+//			Scanner tweetParts = new Scanner(line).useDelimiter(
+//					"\\s*created_at:\\s*|\\s*location:\\s*|\\s*screen_name:\\s*|\\s*description:\\s*|\\s*hashtags:\\s*|\\s*url:\\s*|\\s*pageTitle:\\s*");
+//			if (tweetParts == null) {
+//				System.out.println("wat");
+//			}
+//			JSONObject obj = new JSONObject(line);
+			Scanner tweetParts = new Scanner(line);
+			tweetParts.useDelimiter("created_at:");
+//			System.out.println("created_at " + tweetParts.next());
+//			tweetParts.
+			System.out.println(tweetParts.next());
+//			String date = tweetParts.next();
+//			String coords = tweetParts.next();
+//			String user = tweetParts.next();
+//			String text = tweetParts.next();
+//			String hashtags = tweetParts.next();
+//			String url = tweetParts.next();
+//			String ptitle = tweetParts.next();
+//			Tweet tweet = new Tweet(date, coords, user, text, hashtags, url, ptitle);
 			/**
-			 * It's just not doing anything@!
+			 * Struct of tweet in JSON files created_at, location, screen_name, full_text,
+			 * hashtags, url, pageTitle
 			 */
+//			Tweet DUMMY_TWEET = new Tweet("Sat May 09 01:50:31 +0000 2020", "Rowland Heights, CA", "RowlandRaiders",
+//					"Senior Cyrille Mae Naces will be enlisting in the Navy - Thank you for your service! https://t.co/0u6TJnnfOl @rowlandhs @RowlandSchools #RowlandRaiders #WeAreRUSD https://t.co/HOkkEtNojw",
+//					"", "https://t.co/yvGFGmAckf", "Rowland High School");
 //			System.out.println(date + coords + user + text + hashtags + url + ptitle);
-			addTweet(tweet);
+//			addTweet(tweet);
+//			addTweet(DUMMY_TWEET);
+//			System.out.println(DUMMY_TWEET.ptitle);
 //	       	tweetParts.close();
 		}
 	}
 
-public static void addTweet(Tweet tweet) {
-  IndexWriter index = null;
+	public static void addTweet(Tweet tweet) {
+		IndexWriter index = null;
 //  File f = new File("")
-  try {
-    IndexWriterConfig indexConfig = new IndexWriterConfig(Version.LUCENE_34, new StandardAnalyzer( Version.LUCENE_35));
-    index = new IndexWriter(FSDirectory.open(new File("index")), indexConfig);
-
+		try {
+			IndexWriterConfig indexConfig = new IndexWriterConfig(Version.LUCENE_34,
+					new StandardAnalyzer(Version.LUCENE_35));
+			index = new IndexWriter(FSDirectory.open(new File("index")), indexConfig);
 			Document doc = new Document();
-    doc.add(new Field("date", tweet.date, Field.Store.YES, Field.Index.NO));
-    doc.add(new Field("user", tweet.user, Field.Store.YES, Field.Index.NO));
-    doc.add(new Field("coords", tweet.coords, Field.Store.YES, Field.Index.NO));
-    doc.add(new Field("text", tweet.text, Field.Store.YES, Field.Index.ANALYZED));
-    doc.add(new Field("hashtags", tweet.hashtags, Field.Store.YES, Field.Index.ANALYZED));
-    doc.add(new Field("link", tweet.link, Field.Store.YES, Field.Index.NO));
-    doc.add(new Field("ptitle", tweet.ptitle, Field.Store.YES, Field.Index.ANALYZED));
-    index.addDocument(doc);
-  } catch (Exception e) {
-    e.printStackTrace();
-  }
-}
+			doc.add(new Field("date", tweet.date, Field.Store.YES, Field.Index.NO));
+			doc.add(new Field("user", tweet.user, Field.Store.YES, Field.Index.NO));
+			doc.add(new Field("coords", tweet.coords, Field.Store.YES, Field.Index.NO));
+			doc.add(new Field("text", tweet.text, Field.Store.YES, Field.Index.ANALYZED));
+			doc.add(new Field("hashtags", tweet.hashtags, Field.Store.YES, Field.Index.ANALYZED));
+			doc.add(new Field("link", tweet.link, Field.Store.YES, Field.Index.NO));
+			doc.add(new Field("ptitle", tweet.ptitle, Field.Store.YES, Field.Index.ANALYZED));
+			index.addDocument(doc);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-public static String[] search(String queryStr, int k) throws CorruptIndexException, IOException {
-  IndexReader reader = IndexReader.open(FSDirectory.open(new File("index")));
-  IndexSearcher searcher = new IndexSearcher(reader);
-  QueryParser parser = new QueryParser(Version.LUCENE_34, "text", new StandardAnalyzer(Version.LUCENE_35));
+	public static String[] search(String queryStr, int k) throws CorruptIndexException, IOException {
+		IndexReader reader = IndexReader.open(FSDirectory.open(new File("index")));
+		IndexSearcher searcher = new IndexSearcher(reader);
+		QueryParser parser = new QueryParser(Version.LUCENE_34, "text", new StandardAnalyzer(Version.LUCENE_35));
 
-  try {
-    StringTokenizer tokenizer = new StringTokenizer(queryStr," ~`!@#$%^&*()_-+={[}]|:;'<>,./?\"\'\\/\n\t\b\f\r");
+		try {
+			StringTokenizer tokenizer = new StringTokenizer(queryStr,
+					" ~`!@#$%^&*()_-+={[}]|:;'<>,./?\"\'\\/\n\t\b\f\r");
 
-    String parseable = "";
-    while ( tokenizer.hasMoreElements() ) {
-      String token = tokenizer.nextToken();
-      parseable += "text:" + token + "^1" + "hashtags:" + token + "^1.5" + "ptitle:" + token + "^2.0";
-    }
+			String parseable = "";
+			while (tokenizer.hasMoreElements()) {
+				String token = tokenizer.nextToken();
+				parseable += "text:" + token + "^1" + "hashtags:" + token + "^1.5" + "ptitle:" + token + "^2.0";
+			}
 
-    Query query = parser.parse(parseable);
+			Query query = parser.parse(parseable);
 
-    TopDocs result = searcher.search(query, k);
+			TopDocs result = searcher.search(query, k);
 
-    String[] tweets = new String[result.scoreDocs.length];
-    for (int i = 0; i < result.scoreDocs.length; i++) {
-      String tweet = "@" + searcher.doc(result.scoreDocs[i].doc).getFieldable("user").stringValue();
-      String date = searcher.doc(result.scoreDocs[i].doc).getFieldable("date").stringValue();
-      tweet += ": " + searcher.doc(result.scoreDocs[i].doc).getFieldable("text").stringValue();
-      tweet += "<br/>" + date + " score: " + result.scoreDocs[i].score;
-      tweets[i] = tweet;
-    }
+			String[] tweets = new String[result.scoreDocs.length];
+			for (int i = 0; i < result.scoreDocs.length; i++) {
+				String tweet = "@" + searcher.doc(result.scoreDocs[i].doc).getFieldable("user").stringValue();
+				String date = searcher.doc(result.scoreDocs[i].doc).getFieldable("date").stringValue();
+				tweet += ": " + searcher.doc(result.scoreDocs[i].doc).getFieldable("text").stringValue();
+				tweet += "<br/>" + date + " score: " + result.scoreDocs[i].score;
+				tweets[i] = tweet;
+			}
 
-    return tweets;
+			return tweets;
 
-  } catch (Exception e) {
-    e.printStackTrace();
-  }
-return null;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 
-}
+	}
 }
