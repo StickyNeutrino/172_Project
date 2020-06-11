7b63a8c508423d74b07844fc4065ed9a9f649cce
StickyNeutrino <dfitc001@ucr.edu>
2020-06-11 00:27:53 +0000

    Tweet class

--- lucene/App.java
+++ lucene/App.java
@@ -1,6 +1,26 @@
 //package TweetLucene;
 
 
+class Tweet {
+  public String date;
+  public String coords;
+  public String user;
+  public String text;
+  public String hashtags;
+  public String link;
+  public String ptitle;
+
+  Tweet(String d, String c, String u, String t, String h, String l, String p) {
+    this.date = d;
+    this.coords = c;
+    this.user = u;
+    this.text = t;
+    this.hashtags = h;
+    this.link = l;
+    this.ptitle = p;
+  }
+}
+
 public class App
 {
     public static void main( String[] args )
