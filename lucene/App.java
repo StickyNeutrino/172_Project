bf5ef0247741665bc1b9ea5987089e0d7a8b9292
StickyNeutrino <dfitc001@ucr.edu>
2020-06-11 00:16:45 +0000

    Remove mongo db

--- lucene/App.java
+++ lucene/App.java
@@ -1,22 +1,11 @@
 //package TweetLucene;
 
-import com.mongodb.MongoClient;
-
-
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
-
-import org.bson.Document;
-import java.util.Arrays;
-
 
 public class App
 {
     public static void main( String[] args )
     {
-        MongoClient mongoClient = new MongoClient( "mongo" );
-        MongoDatabase database = mongoClient.getDatabase("tweets");
-        MongoCollection<Document> collection = database.getCollection("test");
+
         System.out.println("hi");
     }
 }
